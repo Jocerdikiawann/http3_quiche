2129629701ba7e0861407580ef20bfbd33c71ca7
Jocerdikiawann <cocokcocok33@gmail.com>
2025-08-30 16:53:24 +0700

    Clean up comment and function

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate log;
 
-use std::{collections::HashMap, net};
+use std::{collections::HashMap, io, net};
 
 use example_http3::model::client::{Client, ClientMap};
 use ring::rand::*;
@@ -10,46 +10,32 @@ use quiche::h3::NameValue;
 
 const MAX_DATAGRAM_SIZE: usize = 1350;
 
-fn main() {
+fn main() -> io::Result<()> {
     let mut stream_states: HashMap<u64, (Vec<quiche::h3::Header>, Vec<u8>)> = HashMap::new(); // (headers, body_buf)
 
     let mut buf = [0; 65535];
     let mut out = [0; MAX_DATAGRAM_SIZE];
 
-    let mut args = std::env::args();
-
-    let cmd = &args.next().unwrap();
-
-    if args.len() != 0 {
-        println!("Usage: {cmd}");
-        println!("\nSee tools/apps/ for more complete implementations.");
-        return;
-    }
-
     // Setup the event loop.
-    let mut poll = mio::Poll::new().unwrap();
+    let mut poll = mio::Poll::new()?;
     let mut events = mio::Events::with_capacity(1024);
 
     // Create the UDP listening socket, and register it with the event loop.
-    let mut socket = mio::net::UdpSocket::bind("127.0.0.1:4433".parse().unwrap()).unwrap();
+    let mut socket = mio::net::UdpSocket::bind("127.0.0.1:4433".parse().unwrap())?;
     poll.registry()
-        .register(&mut socket, mio::Token(0), mio::Interest::READABLE)
-        .unwrap();
+        .register(&mut socket, mio::Token(0), mio::Interest::READABLE)?;
 
     // Create the configuration for the QUIC connections.
     let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION).unwrap();
-
     config
         .load_cert_chain_from_pem_file("src/bin/cert.crt")
         .unwrap();
     config
         .load_priv_key_from_pem_file("src/bin/cert.key")
         .unwrap();
-
     config
         .set_application_protos(quiche::h3::APPLICATION_PROTOCOL)
         .unwrap();
-
     config.set_max_idle_timeout(5000);
     config.set_max_recv_udp_payload_size(MAX_DATAGRAM_SIZE);
     config.set_max_send_udp_payload_size(MAX_DATAGRAM_SIZE);
@@ -72,24 +58,13 @@ fn main() {
     let local_addr = socket.local_addr().unwrap();
 
     loop {
-        // Find the shorter timeout from all the active connections.
-        //
-        // TODO: use event loop that properly supports timers
         let timeout = clients.values().filter_map(|c| c.conn.timeout()).min();
+        poll.poll(&mut events, timeout)?;
 
-        poll.poll(&mut events, timeout).unwrap();
-
-        // Read incoming UDP packets from the socket and feed them to quiche,
-        // until there are no more packets to read.
         'read: loop {
-            // If the event loop reported no events, it means that the timeout
-            // has expired, so handle it without attempting to read packets. We
-            // will then proceed with the send loop.
             if events.is_empty() {
                 debug!("timed out");
-
                 clients.values_mut().for_each(|c| c.conn.on_timeout());
-
                 break 'read;
             }
 
@@ -97,8 +72,6 @@ fn main() {
                 Ok(v) => v,
 
                 Err(e) => {
-                    // There are no more UDP packets to read, so end the read
-                    // loop.
                     if e.kind() == std::io::ErrorKind::WouldBlock {
                         debug!("recv() would block");
                         break 'read;
@@ -128,8 +101,6 @@ fn main() {
             let conn_id = &conn_id.as_ref()[..quiche::MAX_CONN_ID_LEN];
             let conn_id = conn_id.to_vec().into();
 
-            // Lookup a connection based on the packet's connection ID. If there
-            // is no connection matching, create a new one.
             let client = if !clients.contains_key(&hdr.dcid) && !clients.contains_key(&conn_id) {
                 if hdr.ty != quiche::Type::Initial {
                     error!("Packet is not Initial");
@@ -159,10 +130,8 @@ fn main() {
 
                 let scid = quiche::ConnectionId::from_ref(&scid);
 
-                // Token is always present in Initial packets.
                 let token = hdr.token.as_ref().unwrap();
 
-                // Do stateless retry if the client didn't send a token.
                 if token.is_empty() {
                     warn!("Doing stateless retry");
 
@@ -193,8 +162,6 @@ fn main() {
 
                 let odcid = validate_token(&from, token);
 
-                // The token was not valid, meaning the retry failed, so
-                // drop the packet.
                 if odcid.is_none() {
                     error!("Invalid address validation token");
                     continue 'read;
@@ -205,8 +172,6 @@ fn main() {
                     continue 'read;
                 }
 
-                // Reuse the source connection ID we sent in the Retry packet,
-                // instead of changing it again.
                 let scid = hdr.dcid.clone();
 
                 debug!("New connection: dcid={:?} scid={:?}", hdr.dcid, scid);
@@ -236,7 +201,6 @@ fn main() {
                 from,
             };
 
-            // Process potentially coalesced packets.
             let read = match client.conn.recv(pkt_buf, recv_info) {
                 Ok(v) => v,
 
@@ -248,8 +212,6 @@ fn main() {
 
             debug!("{} processed {} bytes", client.conn.trace_id(), read);
 
-            // Create a new HTTP/3 connection as soon as the QUIC connection
-            // is established.
             if (client.conn.is_in_early_data() || client.conn.is_established())
                 && client.http3_conn.is_none()
             {
@@ -268,20 +230,17 @@ fn main() {
                         }
                     };
 
-                // TODO: sanity check h3 connection before adding to map
                 client.http3_conn = Some(h3_conn);
             }
 
             if client.http3_conn.is_some() {
-                // Handle writable streams.
                 for stream_id in client.conn.writable() {
                     handle_writable(client, stream_id);
                 }
 
-                // Process HTTP/3 events.
-                loop {
-                    let http3_conn = client.http3_conn.as_mut().unwrap();
+                let http3_conn = client.http3_conn.as_mut().unwrap();
 
+                loop {
                     match http3_conn.poll(&mut client.conn) {
                         Ok((stream_id, quiche::h3::Event::Headers { list, .. })) => {
                             handle_request_headers(
@@ -292,7 +251,6 @@ fn main() {
                                 &mut stream_states,
                             );
                         }
-
                         Ok((stream_id, quiche::h3::Event::Data)) => {
                             handle_request_body(
                                 stream_id,
@@ -301,29 +259,20 @@ fn main() {
                                 &mut stream_states,
                             );
                         }
-
-                        Ok((_stream_id, quiche::h3::Event::Finished)) => {
+                        Ok((stream_id, quiche::h3::Event::Finished)) => {
                             handle_stream_finished(
-                                _stream_id,
+                                stream_id,
                                 http3_conn,
                                 &mut client.conn,
                                 &mut stream_states,
                             );
                         }
-
-                        Ok((_stream_id, quiche::h3::Event::Reset { .. })) => (),
-
-                        Ok((_prioritized_element_id, quiche::h3::Event::PriorityUpdate)) => (),
-
-                        Ok((_goaway_id, quiche::h3::Event::GoAway)) => (),
-
-                        Err(quiche::h3::Error::Done) => {
-                            break;
-                        }
-
+                        Ok((_stream_id, quiche::h3::Event::Reset { .. })) => {}
+                        Ok((_prioritized_element_id, quiche::h3::Event::PriorityUpdate)) => {}
+                        Ok((_goaway_id, quiche::h3::Event::GoAway)) => {}
+                        Err(quiche::h3::Error::Done) => break,
                         Err(e) => {
                             error!("{} HTTP/3 error {:?}", client.conn.trace_id(), e);
-
                             break;
                         }
                     }
@@ -331,9 +280,6 @@ fn main() {
             }
         }
 
-        // Generate outgoing QUIC packets for all active connections and send
-        // them on the UDP socket, until quiche reports that there are no more
-        // packets to be sent.
         for client in clients.values_mut() {
             loop {
                 let (write, send_info) = match client.conn.send(&mut out) {
@@ -445,14 +391,6 @@ fn handle_stream_finished(
     }
 }
 
-/// Generate a stateless retry token.
-///
-/// The token includes the static string `"quiche"` followed by the IP address
-/// of the client and by the original destination connection ID generated by the
-/// client.
-///
-/// Note that this function is only an example and doesn't do any cryptographic
-/// authenticate of the token. *It should not be used in production system*.
 fn mint_token(hdr: &quiche::Header, src: &net::SocketAddr) -> Vec<u8> {
     let mut token = Vec::new();
 
@@ -469,13 +407,6 @@ fn mint_token(hdr: &quiche::Header, src: &net::SocketAddr) -> Vec<u8> {
     token
 }
 
-/// Validates a stateless retry token.
-///
-/// This checks that the ticket includes the `"quiche"` static string, and that
-/// the client IP address matches the address stored in the ticket.
-///
-/// Note that this function is only an example and doesn't do any cryptographic
-/// authenticate of the token. *It should not be used in production system*.
 fn validate_token<'a>(src: &net::SocketAddr, token: &'a [u8]) -> Option<quiche::ConnectionId<'a>> {
     if token.len() < 6 {
         return None;
@@ -521,6 +452,7 @@ fn build_response(
 
     let (status, body) = match method {
         "GET" => (200, b"Hello World! From GET".to_vec()),
+
         "POST" => {
             if let Some(body) = request_body {
                 println!("You received POST body: {:?}", std::str::from_utf8(body));
@@ -529,6 +461,7 @@ fn build_response(
                 (400, b"Missing body for POST".to_vec())
             }
         }
+
         "PUT" => {
             if let Some(body) = request_body {
                 println!("You received PUT body: {:?}", std::str::from_utf8(body));
